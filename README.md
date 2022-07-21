fbc66995877c8d6b8d9c52b4eab47eefab0d5873
olli <65198941+knokbak@users.noreply.github.com>
2022-07-21 21:18:46 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,140 @@
+# CF-JWT
+### A JWT library for Cloudflare Workers. Compliant with [RFC 7519](https://tools.ietf.org/html/rfc7519).
+
+## What are JWTs?
+JWTs (or "JSON Web Tokens") are an easy way to issue and verify information tokens on-the-fly. JWTs can be signed by one party (an "issuer"), then verified by another (the "audience").
+
+## What are JWTs good for?
+- Most short-lived one-use authentication tokens.
+- Long-lives tokens that can be revoked or blacklisted.
+- General data storage where data should not be editable by the user.
+
+## What should JWTs NOT be used for?
+- Long-lived authentication tokens, or authentication tokens that can be reused.
+- Any type of authentication token that cannot be revoked or blacklisted.
+- To store any sensitive data - unless you are encrypting the JWT. If you are, you *should* be OK.
+
+## How do I use this library?
+
+### Importing the library
+```ts
+import JWT from 'cf-jwt';
+// or, if you're still old-school:
+const JWT = require('cf-jwt');
+```
+
+### Creating a JWT
+```ts
+const jwt = await JWT.sign({
+    hello: 'world!',
+    some: 'lovely data',
+    that: 'i can sign!',
+}, 'what-a-lovely-secret', {
+    algorithm: 'HS256',
+    expiresAt: new Date(Date.now() + 60000), // 60 seconds
+});
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJoZWxsbyI6IndvcmxkISIsInNvbWUiOiJsb3ZlbHkgZGF0YSIsInRoYXQiOiJpIGNhbiBzaWduISIsImlhdCI6MTY1ODQzMzEyNywiZXhwIjoxNjU4NDMzMTg4fQ.u5ejuvjb_t0F2uQUcOGfwtcxUpgIdpQEBQEdwDF7gQE
+```
+
+### Verifying a JWT
+```ts
+const jwt = await JWT.verify(jwtString, 'what-a-lovely-secret', {
+    algorithms: ['HS256'],
+});
+/**
+ * {
+ *     hello: 'world!',
+ *     some: 'lovely data',
+ *     that: 'i can sign!',
+ *     iat: 2022-07-21T19:54:55.000Z,
+ *     exp: 2022-07-21T19:55:56.000Z
+ * }
+ */
+```
+
+## API Reference
+
+### JWT.sign(payload: any, secret: string, options: JWTSignOptions): Promise\<string\>
+### JWT.signSync(payload: any, secret: string, options: JWTSignOptions): string
+Signs an object and returns a JWT string.
+
+```ts
+const uuid = crypto.randomUUID();
+const signedJWT = await JWT.sign({
+    grant: 'access to my cookie jar',
+}, 'chocolate chip cookies!!!!!', {
+    algorithm: 'HS256', // in most cases, this should be sufficient
+    expiresAt: new Date(Date.now() + (60000 * 60)), // expires in 60 minutes
+    notBefore: new Date(Date.now() + 30000), // becomes valid in 30 seconds
+    audience: 'the entire world of course!',
+    issuer: 'me :D',
+    jwtid: uuid, // make sure this is random!
+    subject: 'my best friend',
+});
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJncmFudCI6ImFjY2VzcyB0byBteSBjb29raWUgamFyIiwiaWF0IjoxNjU4NDM0MDk1LCJleHAiOjE2NTg0Mzc2OTYsIm5iZiI6MTY1ODQzNDEyNSwiYXVkIjoidGhlIGVudGlyZSB3b3JsZCBvZiBjb3Vyc2UhIiwiaXNzIjoibWUgOkQiLCJqdGkiOiI0OGY3ODg4Yy1mMmVkLTRhODAtOWNjMS1hZmQzYzI0ZGY0ZTMiLCJzdWIiOiJteSBiZXN0IGZyaWVuZCJ9.-IaNMkkf2lYyMGQC737Ig1yxr0K6bh3POraBAToDSAc
+```
+
+#### Parameters
+- `payload` (`Object`): The object to sign.
+- `secret` (`string`): The secret to use to sign the object.
+- `options` (`Object`): An object containing options for the JWT.
+- `options.algorithm` (`string`): The algorithm to use to sign the object. Available options are `HS256` (SHA-256), `HS384` (SHA-384) and `HS512` (SHA-512).
+- `options.expiresAt`? (`Date`): The time at which the JWT should expire. Compliant libraries and clients should reject the JWT if it is used AFTER this date. Defaults to `null`.
+- `options.notBefore`? (`Date`): The time before which the JWT should not be accepted. Compliant libraries and clients should reject the JWT if it is used BEFORE this date. Defaults to `null`.
+- `options.audience`? (`string`): The audience of the JWT. This is used to tell compliant libraries and clients who the JWT is meant to be accepted by. Defaults to `null`.
+- `options.issuer`? (`string`): The issuer of the JWT. This is used to tell compliant libraries and clients who created the JWT. Defaults to `null`.
+- `options.jwtid`? (`string`): The JWT's unique identifier. If set, you should ensure this value is unique. Defaults to `null`.
+- `options.subject`? (`string`): The subject of the JWT. Defaults to `null`.
+
+**Returns:** The new JWT string.
+**Throws:** An error if one or more incorrect parameters are provided.
+
+### JWT.verify(jwt: string, secret: string, options: JWTSignOptions): Promise\<string\>
+### JWT.verifySync(jwt: string, secret: string, options: JWTSignOptions): string
+Verifies a JWT string and returns the payload as an object.
+
+```ts
+const jwt = await JWT.verify(signedJWT, 'chocolate chip cookies!!!!!', {
+    algorithms: ['HS256'], // list all the algorithms you want to allow
+    audience: ['the entire world of course!'], // list all the audiences you want to allow
+    issuer: ['me :D'], // list all the issuers you want to allow
+    jwtid: [uuid], // list all the jwtids you want to allow
+    subject: ['my best friend'], // list all the subjects you want to allow
+});
+```
+
+#### Parameters
+- `jwt` (`string`): The JWT to verify.
+- `secret` (`string`): The secret to use to verify the JWT.
+- `options` (`Object`): An object containing options for the verification process.
+- `options.algorithms` (`string[]`): A list of all algorithms to allow. Available options are `HS256` (SHA-256), `HS384` (SHA-384) and `HS512` (SHA-512).
+- `options.audience`? (`string` | `string[]`): The audiences to allow for this JWT.
+- `options.issuer`? (`string` | `string[]`): The issuers to allow for this JWT.
+- `options.jwtid`? (`string` | `string[]`): The allowed set of JWT IDs.
+- `options.subject`? (`string` | `string[]`): The subjects allowed for this JWT.
+
+**Returns:** The payload of the JWT.
+**Throws:** An error if one or more issues are found with the JWT.
+
+## MIT License
+```
+Copyright (c) 2022 https://github.com/knokbak
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```
---
+++ src/JWT.ts
@@ -0,0 +1,388 @@
+/*
+ * Copyright (c) 2022 https://github.com/knokbak
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import { Algorithm } from './types';
+import { algo as CryptoJS, enc as CryptoJSEncoders } from 'crypto-js';
+
+export default class JWT {
+    public static signSync(
+        payload: any,
+        secret: string,
+        options: {
+            algorithm: Algorithm;
+            expiresAt?: Date;
+            notBefore?: Date;
+            audience?: string;
+            issuer?: string;
+            jwtid?: string;
+            subject?: string;
+        }
+    ): string {
+        if (!payload) {
+            throw new Error('payload is required');
+        }
+        if (typeof payload !== 'object') {
+            throw new Error('payload must be an object');
+        }
+        if (!secret) {
+            throw new Error('secret is required');
+        }
+        if (typeof secret !== 'string') {
+            throw new Error('secret must be a string');
+        }
+        if (!options) {
+            throw new Error('options is required');
+        }
+        if (typeof options !== 'object') {
+            throw new Error('options must be an object');
+        }
+        if (!options.algorithm) {
+            throw new Error('options.algorithm is required');
+        }
+
+        const header = {
+            alg: options.algorithm,
+            typ: 'JWT',
+        };
+        payload = JWT.buildPayload(payload, options);
+
+        const encodedHeader = JWT.encodeSegment(header);
+        const encodedPayload = JWT.encodeSegment(payload);
+        const segments: string[] = [encodedHeader, encodedPayload];
+        const signed = JWT.signData(
+            segments.join('.'),
+            secret,
+            options.algorithm
+        );
+        segments.push(signed);
+
+        return segments.join('.');
+    }
+
+    public static sign(
+        payload: any,
+        secret: string,
+        options: {
+            algorithm: Algorithm;
+            expiresAt?: Date;
+            notBefore?: Date;
+            audience?: string;
+            issuer?: string;
+            jwtid?: string;
+            subject?: string;
+        }
+    ): Promise<string> {
+        return new Promise((resolve, reject) => {
+            try {
+                const token = JWT.signSync(payload, secret, options);
+                resolve(token);
+            } catch (err) {
+                reject(err);
+            }
+        });
+    }
+
+    public static verifySync(
+        token: string,
+        secret: string,
+        options?: {
+            algorithms?: Algorithm[];
+            audience?: string | string[];
+            issuer?: string | string[];
+            jwtid?: string | string[];
+            subject?: string | string[];
+            clockTolerance?: number;
+        }
+    ): any {
+        options = options || {};
+        if (!options.clockTolerance) {
+            options.clockTolerance = 0;
+        }
+
+        if (!token) {
+            throw new Error('token is required');
+        }
+        if (typeof token !== 'string') {
+            throw new Error('token must be a string');
+        }
+        if (!secret) {
+            throw new Error('secret is required');
+        }
+        if (typeof secret !== 'string') {
+            throw new Error('secret must be a string');
+        }
+
+        const segments = token.split('.');
+        if (segments.length !== 3) {
+            throw new Error(
+                'Tokens must have 3 segments separated by a dot (header.payload.signature). See https://tools.ietf.org/html/rfc7519#section-4.1.'
+            );
+        }
+        const header = JWT.decodeSegment(segments[0]);
+        const payload = JWT.decodeSegment(segments[1]);
+        const signature = segments[2];
+        const issues = [];
+
+        if (
+            options.algorithms &&
+            options.algorithms.indexOf(header.alg) === -1
+        ) {
+            issues.push(
+                `The token\'s algorithm is not explicitly allowed in the list of allowed algorithms. Used: ${
+                    header.alg
+                }. Allowed: [${options.algorithms.join(', ')}].`
+            );
+        }
+        if (options.audience) {
+            if (Array.isArray(options.audience)) {
+                if (options.audience.indexOf(payload.aud) === -1) {
+                    issues.push(
+                        `The token\'s audience is not allowed. Used: ${
+                            payload.aud
+                        }. Allowed: [${options.audience.join(', ')}].`
+                    );
+                }
+            } else {
+                if (payload.aud !== options.audience) {
+                    issues.push(
+                        `The token\'s audience is not allowed. Used: ${payload.aud}. Allowed: ${options.audience}.`
+                    );
+                }
+            }
+        }
+        if (options.issuer) {
+            if (Array.isArray(options.issuer)) {
+                if (options.issuer.indexOf(payload.iss) === -1) {
+                    issues.push(
+                        `The token\'s issuer is not explicitly allowed in the list of allowed issuers. Used: ${
+                            payload.iss
+                        }. Allowed: [${options.issuer.join(', ')}].`
+                    );
+                }
+            } else {
+                if (payload.iss !== options.issuer) {
+                    issues.push(
+                        `The token\'s issuer does not match the expected issuer. Used: ${payload.iss}. Expected: ${options.issuer}.`
+                    );
+                }
+            }
+        }
+        if (options.jwtid) {
+            if (Array.isArray(options.jwtid)) {
+                if (options.jwtid.indexOf(payload.jti) === -1) {
+                    issues.push(
+                        `The token\'s jwtid is not explicitly allowed in the list of allowed jwtids. Used: ${
+                            payload.jti
+                        }. Allowed: [${options.jwtid.join(', ')}].`
+                    );
+                }
+            } else {
+                if (payload.jti !== options.jwtid) {
+                    issues.push(
+                        `The token\'s jwtid does not match the expected jwtid. Used: ${payload.jti}. Expected: ${options.jwtid}.`
+                    );
+                }
+            }
+        }
+        if (options.subject) {
+            if (Array.isArray(options.subject)) {
+                if (options.subject.indexOf(payload.sub) === -1) {
+                    issues.push(
+                        `The token\'s subject is not explicitly allowed in the list of allowed subjects. Used: ${
+                            payload.sub
+                        }. Allowed: [${options.subject.join(', ')}].`
+                    );
+                }
+            } else if (payload.sub !== options.subject) {
+                issues.push(
+                    `The token\'s subject does not match the expected subject. Used: ${payload.sub}. Expected: ${options.subject}.`
+                );
+            }
+        }
+        if (!isNaN(payload.exp)) {
+            if (
+                payload.exp + options.clockTolerance <
+                Math.floor(Date.now() / 1000)
+            ) {
+                issues.push(
+                    `The token has expired. It was valid until ${new Date(
+                        payload.exp * 1000
+                    ).toString()}.`
+                );
+            }
+            payload.exp = new Date(payload.exp * 1000);
+        }
+        if (!isNaN(payload.nbf)) {
+            if (payload.nbf > Math.floor(Date.now() / 1000)) {
+                issues.push(
+                    `The token is not yet valid. It is not valid until ${new Date(
+                        payload.nbf * 1000
+                    ).toString()}.`
+                );
+            }
+            payload.nbf = new Date(payload.nbf * 1000);
+        }
+        if (!isNaN(payload.iat)) {
+            payload.iat = new Date(payload.iat * 1000);
+        }
+        if (!JWT.verifySignature(signature, secret, segments, header)) {
+            issues.push(
+                "The token's signature is invalid. It's likely been modified or manipulated and should not be trusted."
+            );
+        }
+
+        if (issues.length > 0) {
+            throw new Error(
+                `One or more issues were found whilst verifying the JWT:\n${issues.join(
+                    '\n'
+                )}\n`
+            );
+        } else {
+            return payload;
+        }
+    }
+
+    public static verify(
+        token: string,
+        secret: string,
+        options?: {
+            algorithms?: Algorithm[];
+            audience?: string | string[];
+            issuer?: string | string[];
+            jwtid?: string | string[];
+            subject?: string | string[];
+            clockTolerance?: number;
+        }
+    ): Promise<any> {
+        return new Promise((resolve, reject) => {
+            try {
+                const payload = JWT.verifySync(token, secret, options);
+                resolve(payload);
+            } catch (err) {
+                reject(err);
+            }
+        });
+    }
+
+    private static verifySignature(
+        signature: string,
+        secret: string,
+        segments: string[],
+        header: any
+    ): boolean {
+        const algo = JWT.determineAlgorithm(header.alg);
+        const signingInput = segments.slice(0, 2).join('.');
+        const hmac = CryptoJS.HMAC.create(algo, secret);
+        hmac.update(signingInput);
+        const computedSignature = hmac
+            .finalize()
+            .toString(CryptoJSEncoders.Base64);
+        const str = Buffer.from(computedSignature, 'base64').toString(
+            'base64url'
+        );
+        return signature === str;
+    }
+
+    private static decodeSegment(segment: string): any {
+        const str = Buffer.from(segment, 'base64url').toString('utf8');
+        return JSON.parse(str);
+    }
+
+    private static signData(
+        data: any,
+        secret: string,
+        algorithm: Algorithm
+    ): string {
+        const algo = JWT.determineAlgorithm(algorithm);
+        const hmac = CryptoJS.HMAC.create(algo, secret);
+        hmac.update(data);
+        const finalized = hmac.finalize();
+        return Buffer.from(
+            finalized.toString(CryptoJSEncoders.Base64),
+            'base64'
+        ).toString('base64url');
+    }
+
+    private static determineAlgorithm(algorithm: Algorithm): any {
+        switch (algorithm) {
+            case 'HS256':
+                return CryptoJS.SHA256;
+            case 'HS384':
+                return CryptoJS.SHA384;
+            case 'HS512':
+                return CryptoJS.SHA512;
+            default:
+                throw new Error('Unknown algorithm provided!');
+        }
+    }
+
+    private static encodeSegment(segment: any): string {
+        const str = JSON.stringify(segment);
+        return Buffer.from(str, 'utf8').toString('base64url');
+    }
+
+    private static buildPayload(data: any, options: any): any {
+        let payload = data;
+        payload.iat = JWT.getNow();
+        if (options.expiresAt) {
+            if (!(options.expiresAt instanceof Date)) {
+                throw new Error('expiresAt must be a Date');
+            }
+            payload.exp = Math.ceil(options.expiresAt.getTime() / 1000);
+        }
+        if (options.notBefore) {
+            if (!(options.notBefore instanceof Date)) {
+                throw new Error('notBefore must be a Date');
+            }
+            payload.nbf = Math.floor(options.notBefore.getTime() / 1000);
+        }
+        if (options.audience) {
+            if (typeof options.audience !== 'string') {
+                throw new Error('audience must be a string');
+            }
+            payload.aud = options.audience;
+        }
+        if (options.issuer) {
+            if (typeof options.issuer !== 'string') {
+                throw new Error('issuer must be a string');
+            }
+            payload.iss = options.issuer;
+        }
+        if (options.jwtid) {
+            if (typeof options.jwtid !== 'string') {
+                throw new Error('jwtid must be a string');
+            }
+            payload.jti = options.jwtid;
+        }
+        if (options.subject) {
+            if (typeof options.subject !== 'string') {
+                throw new Error('subject must be a string');
+            }
+            payload.sub = options.subject;
+        }
+        return payload;
+    }
+
+    private static getNow(): number {
+        return Math.floor(Date.now() / 1000);
+    }
+}
